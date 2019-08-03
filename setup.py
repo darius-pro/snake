709ddc5697c5d76798238b0ba4178502145bb853
Darius <dariusmehtax@googlemail.com>
2019-08-03 23:09:29 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Snake',  # Required
+    version='0.0.1',  # Required
+    description='A simple python snake game',  # Optional
+    url='https://github.com/darius-pro/snake',  # Optional
+    author='Darius Mehta',  # Optional
+    scripts=['snake'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6', 
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent",
+    ],
+    keywords='snake',  # Optional
+    packages=find_packages(exclude=['tests']),  # Required
+    python_requires='==3.6*,==3.7*',
+    install_requires=['pygame'],  # Optional
+    extras_require={  # Optional
+        'test': ['coverage'],
+    }
+)
